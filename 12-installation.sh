88a65996e59185b5db950dc34b7185d62d798b1f
Nanny <ynavakanth@outlook.com>
2025-05-18 15:14:08 +0530

    program

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,9 +4,18 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]
 then
-echo "Error: : pls run script in root access"
+    echo "Error: : pls run script in root access"
 else
-echo "running in root access" 
+    echo "running in root access" 
 fi
 
-dnf install mysql -y
+    dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "mysql installation success"
+else
+    echo " installation error"
+    exit 1
+
+fi
