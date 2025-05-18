f21686276734f0ab9d2f6343cdff145e0b196428
Nanny <ynavakanth@outlook.com>
2025-05-18 14:51:49 +0530

    program

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -2,15 +2,9 @@
 
 Number=$1
 
-#comparing numbers
-
 if [ $Number -lt 10 ]
 then 
 echo "The $Number is less than 10"
-
-if [ $Number -eq 10 ]
-echo "The number $Number is equl to 10"
-
 else
  echo "The $Number is not less than 10"
  fi
