effef046ed516eb0045fcc06a71d86dfac1f69aa
Nanny <ynavakanth@outlook.com>
2025-05-24 21:33:37 +0530

    program

--- 12-colours.sh
+++ 12-colours.sh
@@ -10,54 +10,54 @@ SCRIPT_NAME=$(echo $0 |cut -d . -f1)
 log_file="$Log_folder/$SCRIPT_NAME.log"
 
 mkdir -p $Log_folder
-echo " script execution started: $(date)" &>>$log_file
+echo " script execution started: $(date)" | tee -a$log_file
 
 if [ $USERID -ne 0 ]
     then
-    echo -e " $R you are running user access, pls run in root access $N" &>>$log_file
+    echo -e " $R you are running user access, pls run in root access $N" | tee -a $log_file
     exit 1
 else
-    echo -e "$G you are running in root access $N" &>>$log_file
+    echo -e "$G you are running in root access $N" | tee -a $log_file
 fi
 
 VALIDATE(){
 if [ $1 eq 0]
    then
-   echo -e "Installing $2 is $G success $N " &>>$log_file
+   echo -e "Installing $2 is $G success $N " | tee -a $log_file
 else
-   echo -e "Installation $R Failure $N " &>>$log_file
+   echo -e "Installation $R Failure $N " | tee -a $log_file
 fi
 }
 
-dnf list installed mysql &>>$log_file
+dnf list installed mysql | tee -a $log_file
 if [ $? -ne 0 ]
    then
-   echo -e " installing mysql $G installing $N " &>>$log_file
-   dnf install mysql -y &>>$log_file
-   VALIDATE $? mysql &>>$log_file
+   echo -e " installing mysql $G installing $N " | tee -a $log_file
+   dnf install mysql -y | tee -a $log_file
+   VALIDATE $? mysql | tee -a $log_file
 else 
-   echo -e " $G Already installed $n " &>>$log_file
+   echo -e " $G Already installed $n " | tee -a $log_file
 
 fi
 
-dnf list installed python3 &>>$log_file
+dnf list installed python3 | tee -a $log_file
 if [ $? -ne 0 ]
    then
-   echo -e " installing python3 $G installing $N "  &>>$log_file
-   dnf install python3 -y &>>$log_file
+   echo -e " installing python3 $G installing $N "  | tee -a $log_file
+   dnf install python3 -y | tee -a $log_file
    VALIDATE $? python3
 else 
-   echo -e " $G Already installed $n " &>>$log_file
+   echo -e " $G Already installed $n " | tee -a $log_file
 
 fi
 
-dnf list installed nginx &>>$log_file
+dnf list installed nginx | tee -a $log_file
 if [ $? -ne 0 ]
    then
-   echo -e " installing nginx $G installing $N " &>>$log_file
-   dnf install nginx -y &>>$log_file
+   echo -e " installing nginx $G installing $N " | tee -a $log_file
+   dnf install nginx -y | tee -a $log_file
    VALIDATE $? nginx
 else 
-   echo -e " $G Already installed $n " &>>$log_files
+   echo -e " $G Already installed $n " | tee -a $log_files
 
 fi
